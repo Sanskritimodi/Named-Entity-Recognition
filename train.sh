c576819c8df6b6835d3e827bd4232409335d7880
Sanskritimodi <103325600+Sanskritimodi@users.noreply.github.com>
2023-08-22 22:45:24 +0530

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+echo "convert iob datafile to flair format"
+
+python -m flair.datasets.convert_to_flair data/gu_train.iob data/
+
+echo "training"
+
+python -m flair.train -c config.cfg --cuda
+
+echo "training completed"
 
