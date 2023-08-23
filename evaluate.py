1067a3b4234717c2c60e6684c7f41994ba0568ec
Sanskritimodi <103325600+Sanskritimodi@users.noreply.github.com>
2023-08-23 20:54:18 +0530

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -9,3 +9,15 @@ with open(eval_file_path, 'r', encoding='utf-8') as file:
 
 eval_docs = [nlp(text) for text in eval_data.split('\n') if text.strip()]
 
+output = []
+
+for doc in eval_docs:
+    for tok in doc:
+        if tok.ent_iob_ != "0":
+            ent = tok.ent_type_
+            label = tok.ent_iob_ + '-' + ent
+            output.append("\t".join([str(tok), label]))
+
+print("\n".join(output).encode("utf-8"))
+
+
