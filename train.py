28c43697c4f99c3c7f5b6c4b29ea7e5c82aed59e
Sanskritimodi <103325600+Sanskritimodi@users.noreply.github.com>
2023-08-28 12:38:36 +0530

    Update train.py

--- train.py
+++ train.py
@@ -21,44 +21,63 @@ label_type = 'ner'
 # Create tag dictionary
 label_dictionary = corpus.make_label_dictionary(label_type=label_type)
 
-# Load IndicBERT Gujarati pretrained model
+''' Load IndicBERT Gujarati pretrained model
 #indicbert_model_name = "l3cube-pune/gujarati-bert"
 #tokenizer = AutoTokenizer.from_pretrained(indicbert_model_name)
 #model = AutoModelForMaskedLM.from_pretrained(indicbert_model_name)
-# Load model directly
-#from transformers import AutoModel
-#model = AutoModel.from_pretrained("ai4bharat/indic-bert")
 
 # Load model directly
-tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
-model = AutoModelForMaskedLM.from_pretrained("bert-base-cased")
+#tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+#model = AutoModelForMaskedLM.from_pretrained("bert-base-cased")'''
 
+embedding_types = [
+    WordEmbeddings('glove'),
+    FlairEmbeddings('news-forward'),
+    FlairEmbeddings('news-backward'),
+]
 
+embeddings = StackedEmbeddings(embeddings=embedding_types)
 
-# Create transformer-based embedding using IndicBERT model
-from flair.embeddings import TransformerWordEmbeddings
+'''Create transformer-based embedding using IndicBERT model
+#from flair.embeddings import TransformerWordEmbeddings
 
 # init embedding
-embedding = TransformerWordEmbeddings(indicbert_model_name)
+#embedding = TransformerWordEmbeddings(indicbert_model_name)
 
 # Initialize sequence tagger with transformer-based embedding
 tagger = SequenceTagger(hidden_size=256,
                         embeddings=StackedEmbeddings(embeddings=[embedding]),
                         tag_dictionary=label_dictionary,
-                        tag_type=label_type)
+                        tag_type=label_type)'''
 
-# Initialize trainer
-trainer = ModelTrainer(tagger, corpus)
+tagger = SequenceTagger(hidden_size=256,
+                        embeddings=embeddings,
+                        tag_dictionary=label_dictionary,
+                        tag_type=label_type)
 
 import time
 for i in ['—','\\','|','/']*10:
      time.sleep(0.2)
      print(f'\rLoading... {i}', end="\r")
 
+# 6. initialize trainer
+trainer = ModelTrainer(tagger, corpus)
+
+# 7. start training
+trainer.train('resources/taggers/ner-english',
+              learning_rate=0.1,
+              mini_batch_size=32,
+              max_epochs=20)
+
+'''# Initialize trainer
+trainer = ModelTrainer(tagger, corpus)
+
+
+
 # Start training
 trainer.train('resources/taggers/ner-english',
               train_with_dev=False,
-              max_epochs=20)
+              max_epochs=20)'''
 
 
 
