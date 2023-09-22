acd9c945edddd391278325e415cc93ed7683f539
Sanskriti Modi <103325600+Sanskritimodi@users.noreply.github.com>
2023-09-22 15:51:14 +0530

    Update bio_to_json.py

--- bio_to_json.py
+++ bio_to_json.py
@@ -2,8 +2,8 @@ from stanza.utils.datasets.ner.utils import convert_bio_to_json
 
 SHARDS = ('train', 'dev', 'test')
 
-base_input_path="/Users/sanskriti/Documents/NAMED ENTITY RECOGNITION/STANZA_NER/data/ner"
-base_output_path="/Users/sanskriti/Documents/NAMED ENTITY RECOGNITION/STANZA_NER/data/ner"
+base_input_path="/data/ner"
+base_output_path="/data/ner"
 short_name="gu_conll"
 
 convert_bio_to_json(base_input_path, base_output_path, short_name, suffix="bio", shard_names=SHARDS, shards=SHARDS)
