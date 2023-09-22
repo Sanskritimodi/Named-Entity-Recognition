82b2d27b76e845d3456b6e03fb7ea49fd9c12cd3
Sanskriti Modi <103325600+Sanskritimodi@users.noreply.github.com>
2023-09-22 12:48:09 +0530

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-gunzip root/ft/cc.gu.300.vec.gz 
+
 python3 -m stanza.models.common.convert_pretrain ~/stanza_resources/gu/pretrain/fasttext.pt ~/root/ft/cc.gu.300.vec 150000
 
 python -m stanza.utils.training.run_ner gu_conll
