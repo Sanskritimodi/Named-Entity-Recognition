2c8e0d397f70c7ac553dac1fa1c7eb000f1fca5c
Sanskriti Modi <103325600+Sanskritimodi@users.noreply.github.com>
2023-09-27 12:50:15 +0530

    adding bioes scheme to training

--- train.sh
+++ train.sh
@@ -2,6 +2,6 @@
 #python3 -m stanza.models.common.convert_pretrain ~/stanza_resources/gu/pretrain/fasttext.pt ~/root/ft/cc.gu.300.vec 150000
 python3 bio_to_json.py
 
-python3 -m stanza.utils.training.run_ner gu_conll
+python3 -m stanza.utils.training.run_ner gu_conll --scheme bioes
 
 
